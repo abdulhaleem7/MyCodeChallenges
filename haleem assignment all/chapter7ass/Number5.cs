407acc16dcefe2052422d374eb2c6ffd5d16cc7a
patience <97902172+abdulhaleem7@users.noreply.github.com>
2023-03-31 06:49:08 +0100

    Update Number5.cs

---
+++ haleem assignment all/chapter7ass/Number5.cs
@@ -0,0 +1,46 @@
+using System;
+namespace chapter7ass
+{
+    class number1
+    {
+        public static void patience5()
+        { 
+            int[] numbers = { 3,4,5,6,7, 2, 3, 4, 2, 2, 4,5,6,7,3 };
+            string number = "";
+            int maximumCount = 0;
+            int currentCount = 1;
+            string num = "";
+            for (int x = 0; x < numbers.Length - 1; x++)
+            {
+                if (numbers[x] - numbers[x + 1] == -1)
+                {
+                    currentCount++;
+                    number += numbers[x];
+                }
+                else
+                {
+
+                    if (currentCount > maximumCount)
+                    {
+                        maximumCount = currentCount;
+                        number += numbers[x];
+                        num = number;
+                    }
+                    number = "";
+                    currentCount = 1;
+                }
+                if(numbers.Length-2 == x)
+                {
+                    if (currentCount > maximumCount)
+                    {
+
+                        maximumCount = currentCount;
+                        number += numbers[x+1];
+                        num = number;
+                    }
+                }
+            }
+            Console.WriteLine(num);
+        }
+    }
+}
